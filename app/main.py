e4270c27dee06ec0b0c8cea63cd24b53e3a3dd63
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-03-19 16:17:38 +0100

    feat: add a new endpoint to generate levels
    Add a new endpoint called `generate` to generate levels from some initial
    input map, now it just mocks it by generating the level from a random input level
    on each call

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from typing import Union
+import numpy as np
 
 from fastapi import FastAPI
+from utils.run_model import run
 
 app = FastAPI()
 
+
+@app.get("/generate")
+def generate():
+    input_map = np.random.randint(0, 7, size=(16, 16)) 
+
+    x = run(0.2, 5, input_map)
+    return {"generated_map": x}
 
 @app.get("/")
 def read_root():
