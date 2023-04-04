cbfaa4c7e51590c25542670059ebf736a74139e9
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-04-04 22:56:30 +0200

    feat: return the 50 steps of the first auxiliary channel back to frontend

--- app/ext/pcgnca/pcgnca/evo/_simulate.py
+++ app/ext/pcgnca/pcgnca/evo/_simulate.py
@@ -68,16 +68,19 @@ def _simulate(
         
         # -- Run the forward pass for n_steps
         levels = [] # keeps track of generated levels after each step
+        aux_channels = []
         for _ in range(n_steps):
 
             # --- Run the single forward pass
-            action = model(in_tensor)
+            action, aux = model(in_tensor)
 
             # --- Extract the level (now 2d int array)
             level = th.argmax(action[0], dim=0).numpy()
 
             # --- Save the levels
             levels.append(level)
+            #aux_channels.append(aux.numpy()) # get all aux channels
+            aux_channels.append(aux.numpy()[0][0]) # for now get only the first aux channel
 
             # --- Setup the input for the model again
             if fixed_tiles is not None and binary_mask is not None:
@@ -101,7 +104,7 @@ def _simulate(
     
     # - Evaluate the batch accordingly
     if to_return == "generated_lvls":
-        return np.array(batch_steps_levels) # n_sols_in_batch x n_steps x dim x dim
+        return np.array(batch_steps_levels), np.array(aux_channels), evaluator.get_zelda_level_stats(level) # n_sols_in_batch x n_steps x dim x dim
     else:
         return evaluator.evaluate_level_batch(batch_stats, to_return)
 
