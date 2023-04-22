86678545b31cca18437d33e181987efb948179d4
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-04-22 16:30:11 +0200

    feat(main.py): add endpoint to get experiment names feat(main.py): add exp_id parameter to generate and get_both_and_obj endpoints refactor(main.py): rename path_length parameter to level_length in generate endpoint refactor(main.py): rename input_map parameter to level_map in generate endpoint

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.utils.run_model import run
 from app.utils.behaviour import get_extremes_behaviour0_char, get_extremes_behaviour1_char, get_behaviour0_char, get_behaviour1_char, get_obj_char
+from app.utils.get_path import list_models_folders
 
 app = FastAPI(title='Interactive NCA API',
               description='API endpoints for the Interactive NCA model')
@@ -33,10 +34,11 @@ def read_root():
 
 # Generate a level from some given seed
 @app.post("/generate")
-async def generate(path_length: float, symmetry: float, input_map: List[List[List[int]]]):
+async def generate(exp_id: int, path_length: float, symmetry: float, input_map: List[List[List[int]]]):
     """
     Generate a level from a given seed
     Args:
+        exp_id: int
         path_length: float
         symmetry: float
         input_map: List[List[int]]
@@ -47,7 +49,7 @@ async def generate(path_length: float, symmetry: float, input_map: List[List[Lis
     input_array = np.array(input_map[0]) # 2D int encoded level
     binary_array = np.array(input_map[1]) # binary array noting which tiles are fixed
     combined = np.array([input_array, binary_array])
-    x = run(symmetry, path_length, combined)
+    x = run(exp_id, symmetry, path_length, combined)
     return {"generated_map": x}
 
 
@@ -93,14 +95,24 @@ def get_behaviour1():
     return {"behaviour1": get_behaviour1_char()}
 
 @app.get("/behaviours")
-def get_both_and_obj():
+def get_both_and_obj(exp_id: int):
     """
     Get the behaviour0 and behaviour1 characteristics and the objective value
 
     Returns:
         behaviours: List[List[float], List[float], List[float]]
     """
-    return {"behaviours": get_obj_char()}
+    return {"behaviours": get_obj_char(exp_id)}
+
+@app.get("/experimentnames")
+def experiment_names():
+    """
+    Get experiment names
+
+    Returns:
+        experiments_names: List
+    """
+    return {"names": list_models_folders()}
 
 # Test endpoints
 @app.get("/test")
