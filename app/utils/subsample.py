196879924538bac1f9ba51c70253df0480eff8e5
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-04-24 00:37:47 +0200

    feat(utils): add subsample_archive and subsample_training_seeds functions
    Add two new functions to the utils module: subsample_archive and subsample_training_seeds. The subsample_archive function takes a path to an archive and a number of models to subsample, and returns a new archive with only the first n_models. The subsample_training_seeds function takes a path to an archive and a path to a pickle file containing training seeds, and returns a new pickle file with only the training seeds that correspond to the subsampled archive.

--- app/utils/subsample.py
+++ app/utils/subsample.py
@@ -1,12 +1,60 @@
 import pandas as pd
+import pickle
 
-df = pd.read_csv("~/Downloads/models_exp11_trained_archive.csv")
+def subsample_archive(archive_path, n_models):
+    """
+    Subsample the archive to the first n_models
 
-# Sort the DataFrame based on "objective" column in descending order
-df_sorted = df.sort_values("objective", ascending=False)
+    Args:
+        archive_path (str): Path to the archive
+        n_models (int): Number of models to subsample
+    """
+    # Load the archive
+    df = pd.read_csv(archive_path)
 
-# Select the first 100 rows of the sorted DataFrame
-df_top100 = df_sorted.head(100).copy()
+    # Sort the DataFrame based on "objective" column in descending order
+    df_sorted = df.sort_values("objective", ascending=False)
 
-# Save the new DataFrame to a CSV file in a folder
-df_top100.to_csv("~/Downloads/trained_archive.csv", index=False)
+    # Select the first n_models rows of the sorted DataFrame
+    df_top = df_sorted.head(n_models).copy()
+
+    # Save the new DataFrame to a CSV file in a folder
+    df_top.to_csv(archive_path, index=False)
+
+def subsample_training_seeds(archive_path, training_seeds_path):
+    """
+    Subsample the training seeds to match the subsampled archive 
+
+    Args:
+      archive_path
+      training_seeds_path
+    """
+    # Load the training seeds
+    with open(training_seeds_path, "rb") as f:
+        train_seeds = pickle.load(f)
+
+    # Subsample the training seeds
+    print("INSIDE")
+    df = pd.read_csv(archive_path)
+
+    generations = df["metadata"].unique()
+
+    subsampled_seeds = {k: v for k, v in train_seeds.items() if k in generations}
+
+    # Save the new training seeds to a pickle file
+    print(df["metadata"])
+    print(subsampled_seeds.keys())
+
+    with open(training_seeds_path, "wb") as f:
+        pickle.dump(subsampled_seeds, f)
+
+
+
+if __name__ == "__main__":
+    # Subsample the archive
+    #subsample_archive("experiments/ExperimentId-6/trained_archive.csv", 100)
+
+    # Subsample the training seeds
+    #subsample_training_seeds("experiments/ExperimentId-6/trained_archive.csv", "experiments/ExperimentId-6/training_seeds.pkl")
+
+    subsample_training_seeds("~/Downloads/models_exp14_trained_archive.csv", "training_seeds.pkl")
---
+++ app/utils/training_seeds.py
@@ -0,0 +1,33 @@
+import numpy as np
+from app.utils.get_path import get_archive, load_training_seeds
+
+def get_training_seeds(exp_id, symmetry, path_length, local):
+    """
+    Find the model and its training seeds 
+
+    Args:
+        symmetry: float
+        path_length: float
+        local: bool
+
+    Returns:
+        List[List[int]]
+    """
+
+    df = get_archive(exp_id, local)
+
+    # - Find the closest model
+    distances = np.sqrt((df["measure_0"] - symmetry)**2 + (df["measure_1"] - path_length)**2)
+    mask = distances == distances.min()
+    generation = df[mask]["metadata"].to_numpy()[0]
+
+    if local:
+        pass
+    else:
+      training_seeds = load_training_seeds(exp_id, local)
+      init_state, fixed_state, binary_mask = training_seeds[generation]["init_states"], training_seeds[generation]["fixed_states"], training_seeds[generation]["binary_mask"]
+
+      np.putmask(init_state, binary_mask, fixed_state)
+
+      return [init_state.tolist(), binary_mask.tolist()]
+
