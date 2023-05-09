3572e71ee699b0d9d76de9b85a33e25219197e6f
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-05-09 20:05:14 +0200

    refactor(training_seeds.py): remove duplicate code and simplify the code to handle different dictionary structures in load_training_seeds() function

--- app/utils/training_seeds.py
+++ app/utils/training_seeds.py
@@ -22,15 +22,17 @@ def get_training_seeds(exp_id, symmetry, path_length, local):
     mask = distances == distances.min()
     generation = df[mask]["metadata"].to_numpy()[0]
 
-    # - Load the training seeds corresponding to the given model
-    training_seeds = load_training_seeds(exp_id, local)
-    init_state, fixed_state, binary_mask = training_seeds[generation]["init_states"], training_seeds[generation]["fixed_states"], training_seeds[generation]["binary_mask"]
 
     # - Apply the fixed tiles
     training_seeds = load_training_seeds(exp_id, local)
-    init_state, fixed_state, binary_mask = training_seeds[generation]["init_states"], training_seeds[generation]["fixed_states"], training_seeds[generation]["binary_mask"]
 
-    if binary_mask is None:
+    # Get the first key of the dictionary to check whether the dictionaries within the dictionary have 3 "fixed_states"
+    first_key = list(training_seeds.keys())[0]
+
+    if len(training_seeds[first_key]) == 3:
+        init_state, fixed_state, binary_mask = training_seeds[generation]["init_states"], training_seeds[generation]["fixed_states"], training_seeds[generation]["binary_mask"]
+    else:
+        init_state = training_seeds[generation]["init_states"]
         binary_mask = np.zeros_like(init_state)
         fixed_state = np.zeros_like(init_state)
 
